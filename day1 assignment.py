cf0fb4bf3d58c954a9341e5c40a7f216f9eefef4
omkar402 <73751902+omkar402@users.noreply.github.com>
2020-11-07 20:45:10 +0530

    DAY 1 ASSIGNMENT SUMBITED

---
+++ day1 assignment.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1.correct english word is 
+
+# B.RAINBOW
+
+# Q2.Write a program which prints “LETS UPGRADE”.
+
+# In[2]:
+
+
+print("LETS UPGRADE")
+
+
+# Q3.Program for profit or loss
+# 
+
+# In[6]:
+
+
+costprice=input()
+sellingprice=input()
+if(costprice<sellingprice):
+    print("Profit")
+elif(costprice>sellingprice):
+     print("Loss")
+else:
+     print("Neither")
+
+
+# Q4.program for takes an amount in Indian Rupees as input.
+
+# In[ ]:
+
+
+rupees=int(input())
+euro=rupees*80
+print(euro)
+
+
+# In[ ]:
+
+
+
+
